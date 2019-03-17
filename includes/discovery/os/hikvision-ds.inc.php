6c4ba2b496ac74e54c8bf65e953566a7f046267b
Spencer Butler <github@crooked.app>
2019-03-16 22:53:37 -0500

    Added support for HikVision-DS Cameras
    clean up

---
+++ includes/discovery/os/hikvision-ds.inc.php
@@ -0,0 +1,7 @@
+<?php
+
+$version   = trim(snmp_get($device, '.1.3.6.1.4.1.39165.1.3.0', '-OQv', 'HIK-DEVICE-MIB'), '"');
+$hardware  = trim(snmp_get($device, '.1.3.6.1.4.1.39165.1.1.0', '-OQv', 'HIK-DEVICE-MIB'), '"');
+$features  = trim(snmp_get($device, '.1.3.6.1.4.1.39165.1.21.0', '-OQv', 'HIK-DEVICE-MIB'), '"');
+$features .= '-' . trim(snmp_get($device, '.1.3.6.1.4.1.39165.1.22.0', '-OQv', 'HIK-DEVICE-MIB'), '"');
+
---
+++ includes/polling/mempools/hikvision-ds.inc.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * hikvision-ds.inc.php
+ *
+ * LibreNMS mempool poller module for hikvision-ds
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    LibreNMS
+ * @link       http://librenms.org
+ * @copyright  2019 Spencer Butler
+ * @author     Spencer Butler <github@crooked.app>
+ */
+
+if ($device['os'] === 'hikvision-ds') {
+  echo 'hikvision-ds:';
+
+  $tmp_data = snmp_get_multi_oid($device, ['.1.3.6.1.4.1.39165.1.10.0', '.1.3.6.1.4.1.39165.1.11.0']);
+  
+  $total = $tmp_data[0];
+  $used  = $tmp_data[1];
+  $mempool['total'] = $total;
+  $mempool['used']  = $used;
+  $mempool['free']  = $total - $used;
+}
